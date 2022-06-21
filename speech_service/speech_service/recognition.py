291a2df4ea8e9603fcfa8bcb58cb5b8bd6d3507b
ItoMasaki <40968251+ItoMasaki@users.noreply.github.com>
2022-06-21 13:37:20 +0900

    Update recognition.py

--- speech_service/speech_service/recognition.py
+++ speech_service/speech_service/recognition.py
@@ -1,49 +1,49 @@
-import rclpy        
-import rclpy.node        
+import rclpy
+import rclpy.node
 from std_msgs.msg import String
 
-import speech_recognition as sr        
+import speech_recognition as sr
 import pyaudio
 
 
-class Recognition(rclpy.node.Node):    
-    def __init__(self):    
-        super().__init__("speech_recognition")    
-    
-        self.get_logger().info('音声認識ノードを起動します')    
-    
-        self.init_rec = sr.Recognizer()    
-    
+class Recognition(rclpy.node.Node):
+    def __init__(self):
+        super().__init__("speech_recognition")
+
+        self.get_logger().info('音声認識ノードを起動します')
+
+        self.init_rec = sr.Recognizer()
+
         self.publisher = self.create_publisher(String, '/speech', 1)
 
     def recognition(self):
-        with sr.Microphone() as source: 
+        with sr.Microphone() as source:
             while True:
                 text = ''
-                
-                audio_data = self.init_rec.record(source, duration=5)    
-                self.get_logger().info(f'音声認識を行います')    
-    
-                try:    
-                    text = self.init_rec.recognize_google(audio_data)    
-    
-                except sr.UnknownValueError:    
-                    pass    
-    
-                msg = String()    
-                msg.data = text    
-                self.get_logger().info(f'認識した音声 "{text}" をトピック名 /speech にパブリッシュします')    
-    
+
+                audio_data = self.init_rec.record(source, duration=5)
+                self.get_logger().info(f'音声認識を行います')
+
+                try:
+                    text = self.init_rec.recognize_google(audio_data)
+
+                except sr.UnknownValueError:
+                    pass
+
+                msg = String()
+                msg.data = text
+                self.get_logger().info(f'認識した音声 "{text}" をトピック名 /speech にパブリッシュします')
+
                 self.publisher.publish(msg)
 
-def main():    
-    rclpy.init()    
-    
-    recognition_node = Recognition()    
-    
+def main():
+    rclpy.init()
+
+    recognition_node = Recognition()
+
     try:
-        recognition_node.recognition()   
+        recognition_node.recognition()
     except:
-        recognition_node.destroy_node()    
-    
+        recognition_node.destroy_node()
+
     rclpy.shutdown()
