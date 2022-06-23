2b09a1821cecc785e66dba84d4e3f1497422876d
ItoMasaki <40968251+ItoMasaki@users.noreply.github.com>
2022-06-23 22:03:29 +0900

    Update speech_service_mpg123.py

--- speech_service/speech_service/speech_service_mpg123.py
+++ speech_service/speech_service/speech_service_mpg123.py
@@ -63,7 +63,9 @@ def main():
 
     speech_service = SpeechService()
 
-    rclpy.spin(speech_service)
-    speech_service.destroy_node()
+    try:
+        rclpy.spin(speech_service)
+    except KeyboardInterrupt:
+        pass
 
     rclpy.shutdown()
