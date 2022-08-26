dc4116a2ed57f0ae8121b4cde81cc4a798ac73d9
Yoshinobu Hagiwara <yhagiwara@em.ci.ritsumei.ac.jp>
2022-08-26 11:10:13 +0900

    Update setup.py

--- speech_service/setup.py
+++ speech_service/setup.py
@@ -16,7 +16,7 @@ setup(
     maintainer='Masaki Ito',
     maintainer_email='ai-robot-book@googlegroups.com',
     description='TODO: Package description',
-    license='Apache-2.0',
+    license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
