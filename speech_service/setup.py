c9b8ea7c5a136aaf720d041563e46204454398da
Yoshinobu Hagiwara <yhagiwara@em.ci.ritsumei.ac.jp>
2022-08-28 22:17:19 +0900

    Update setup.py

--- speech_service/setup.py
+++ speech_service/setup.py
@@ -15,7 +15,7 @@ setup(
     zip_safe=True,
     maintainer='Masaki Ito',
     maintainer_email='ai-robot-book@googlegroups.com',
-    description='TODO: Package description',
+    description='ROS2 package for speech recognition and speech synthesis',
     license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
