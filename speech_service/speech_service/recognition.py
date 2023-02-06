3dddaa585468a7995ae3c0029f98925d2df34333
ItoMasaki <40968251+ItoMasaki@users.noreply.github.com>
2023-02-06 10:19:08 +0900

    コメントの修正
    １．講習会用にコメントを入れていたモノを該当する行の末尾に移動
    ２．テキストと行数が一致するように修正

--- speech_service/speech_service/recognition.py
+++ speech_service/speech_service/recognition.py
@@ -1,49 +1,36 @@
-#[*] PythonからROS2を使用するためのモジュールを読み込みます．
-import rclpy
+import rclpy #[*] PythonからROS2を使用するためのモジュールを読み込みます．
 import rclpy.node
 from std_msgs.msg import String
 
-#[*] 音声認識を行うためのモジュールを読み込みます．
-import speech_recognition as sr
+import speech_recognition as sr #[*] 音声認識を行うためのモジュールを読み込みます．
 
 
-#[*] Recognitionクラスをノードとして使うために，Nodeクラスを継承します．
-class Recognition(rclpy.node.Node):
+class Recognition(rclpy.node.Node): #[*] Recognitionクラスをノードとして使うために，Nodeクラスを継承します．
     def __init__(self):
-        #[*] ノードの名前を'speech_recognition'として登録します．
-        super().__init__('speech_recognition')
+        super().__init__('speech_recognition') #[*] ノードの名前を'speech_recognition'として登録します．
 
-        #[*] ノードが起動したことをloggerを用いて表示します．
-        self.get_logger().info('音声認識ノードを起動します')
+        self.get_logger().info('音声認識ノードを起動します') #[*] ノードが起動したことをloggerを用いて表示します．
 
-        #[*] 音声認識器の初期化を行います．
-        self.init_rec = sr.Recognizer()
+        self.init_rec = sr.Recognizer() #[*] 音声認識器の初期化を行います．
 
-        #[*] 認識した音声をトピックspeechに送るために，Publisherを初期化します．
-        self.publisher = self.create_publisher(String, '/speech', 1)
-        
-        #[*] 5秒ごとに音声認識を行なってほしいので，Timerを初期化します．
-        #[*] 実行する関数はrecognitionです．
-        self.timer = self.create_timer(5.0, self.recognition)
+        self.publisher = self.create_publisher(String, '/speech', 1) #[*] 認識した音声をトピックspeechに送るために，Publisherを初期化します．
+
+        self.timer = self.create_timer(5.0, self.recognition) #[*] 5秒ごとに音声認識を行なってほしいので，Timerを初期化します．実行する関数はrecognitionです．
 
     def recognition(self):
-        #[*] 収音データを扱えるようにします．
-        with sr.Microphone() as source:
+        with sr.Microphone() as source: #[*] 収音データを扱えるようにします．
             text = ''
 
-            #[*] 収音データを5秒間分取り出せるようにします．
-            audio_data = self.init_rec.record(source, duration=5)
+            audio_data = self.init_rec.record(source, duration=5) #[*] 収音データを5秒間分取り出せるようにします．
             self.get_logger().info(f'音声認識を行います')
 
             try:
-                #[*] Googleの音声認識器に収音データを送り，音声認識の結果を受け取ります．
-                text = self.init_rec.recognize_google(audio_data)
+                text = self.init_rec.recognize_google(audio_data) #[*] Googleの音声認識器に収音データを送り，音声認識の結果を受け取ります．
 
             except sr.UnknownValueError:
                 pass
 
-            #[*] 音声認識の結果が得られた場合，speechトピックに音声認識の結果を送ります．
-            if text != '':
+            if text != '': #[*] 音声認識の結果が得られた場合，speechトピックに音声認識の結果を送ります．
                 msg = String()
                 msg.data = text
                 self.get_logger().info(
@@ -53,17 +40,13 @@ class Recognition(rclpy.node.Node):
 
 
 def main():
-    #[*] rclpyを通したrosのコミュニケーションが行えるようにします．
-    rclpy.init()
+    rclpy.init() #[*] rclpyを通したrosのコミュニケーションが行えるようにします．
 
-    #[*] 音声認識のノードを初期化します．
-    recognition_node = Recognition()
+    recognition_node = Recognition() #[*] 音声認識のノードを初期化します．
     
     try:
-        #[*] 音声認識のノードを実行します．
-        rclpy.spin(recognition_node)
+        rclpy.spin(recognition_node) #[*] 音声認識のノードを実行します．
     except KeyboardInterrupt:
         pass
 
-    #[*] rclpyを通したrosのコミュニケーションを終了します．
-    rclpy.shutdown()
+    rclpy.shutdown() #[*] rclpyを通したrosのコミュニケーションを終了します．
