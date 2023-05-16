028b23e1a7557d83d83d9f9cc4aa5da0324c1327
ItoMasaki <40968251+ItoMasaki@users.noreply.github.com>
2023-05-16 13:07:03 +0900

    Update synthesis.py

--- speech_service/speech_service/synthesis.py
+++ speech_service/speech_service/synthesis.py
@@ -1,70 +1,52 @@
-#[*] PythonからROS2を使用するためのモジュールを読み込みます．
-import rclpy
+import rclpy #[*] PythonからROS2を使用するためのモジュールを読み込みます．
 import rclpy.node
 from std_msgs.msg import String
 
-#[*] gTTSはGoogleの音声合成APIを利用できるモジュールです．
-from gtts import gTTS
+from gtts import gTTS #[*] gTTSはGoogleの音声合成APIを利用できるモジュールです．
 
-#[*] 取得した音声合成データを再生するためのモジュールです．
-from io import BytesIO
+from io import BytesIO #[*] 取得した音声合成データを再生するためのモジュールです．
 from mpg123 import Mpg123, Out123
 
 
-#[*] Synthesisクラスをノードとして使うために，Nodeクラスを継承します．
-class Synthesis(rclpy.node.Node):
+class Synthesis(rclpy.node.Node): #[*] Synthesisクラスをノードとして使うために，Nodeクラスを継承します．
     def __init__(self):
-        #[*] ノードの名前を'speech_synthesis'として登録します．
-        super().__init__('speech_synthesis')
+        super().__init__('speech_synthesis') #[*] ノードの名前を'speech_synthesis'として登録します．
 
-        #[*] ノードが起動したことをloggerを用いて表示します．
-        self.get_logger().info('音声合成ノードを起動します')
+        self.get_logger().info('音声合成ノードを起動します') #[*] ノードが起動したことをloggerを用いて表示します．
 
-        #[*] 音声合成してほしい言語の設定します．
-        self.lang = 'en'
-        #[*] 音声合成データを再生するためのクラスを初期化します．
-        self.mp3 = Mpg123()
+        self.lang = 'en' #[*] 音声合成してほしい言語の設定します．
+        self.mp3 = Mpg123() #[*] 音声合成データを再生するためのクラスを初期化します．
         self.out = Out123()
 
-        #[*] 音声合成してほしい文章を取得するためのSubscriptionを初期化します．
-        #[*] 受け取った文章はsynthesis関数で実行されます．
-        self.subscriber = self.create_subscription(
+        self.subscriber = self.create_subscription( #[*] 音声合成してほしい文章を取得するためのSubscriptionを初期化します．受け取った文章はsynthesis関数で実行されます．
             String, '/speech', self.synthesis, 1)
 
     def synthesis(self, msg):
-        #[*] 音声合成してほしい文章を取得した場合
-        if msg.data != '':
+        if msg.data != '': #[*] 音声合成してほしい文章を取得した場合
             self.get_logger().info('音声合成を実行します')
 
             text = msg.data
             self.get_logger().info(f'\"{text}\"と発話します')
 
-            #[*] 音声合成してほしい文章をgoogleの音声合成APIに渡します．
-            tts = gTTS(text, lang=self.lang[:2])
+            tts = gTTS(text, lang=self.lang[:2]) #[*] 音声合成してほしい文章をgoogleの音声合成APIに渡します．
             
-            #[*] 音声合成データを再生するためのIOの初期化を行います．
-            fp = BytesIO()
+            fp = BytesIO() #[*] 音声合成データを再生するためのIOの初期化を行います．
             tts.write_to_fp(fp)
             fp.seek(0)
             self.mp3.feed(fp.read())
 
-            #[*] 音声合成データを再生します．
-            for frame in self.mp3.iter_frames(self.out.start):
+            for frame in self.mp3.iter_frames(self.out.start): #[*] 音声合成データを再生します．
                 self.out.play(frame)
 
 
 def main():
-    #[*] rclpyを通したrosのコミュニケーションが行えるようにします．
-    rclpy.init()
+    rclpy.init() #[*] rclpyを通したrosのコミュニケーションが行えるようにします．
 
-    #[*] 音声認識のノードを初期化します．
-    synthesis_node = Synthesis()
+    synthesis_node = Synthesis() #[*] 音声認識のノードを初期化します．
 
     try:
-        #[*] 音声認識のノードを実行します．
-        rclpy.spin(synthesis_node)
+        rclpy.spin(synthesis_node) #[*] 音声認識のノードを実行します．
     except KeyboardInterrupt:
         pass
 
-    #[*] rclpyを通したrosのコミュニケーションを終了します．
-    rclpy.shutdown()
+    rclpy.shutdown() #[*] rclpyを通したrosのコミュニケーションを終了します．
